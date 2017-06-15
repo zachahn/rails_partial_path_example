8f5466f8be19b9645420a683fa6cf7a083712b12
Zach Ahn <engineering@zachahn.com>
2017-06-15 23:50:34 +0900

    Seeds for articles

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Article.create(name: "Cool story bro", published: true)
+Article.create(name: "Still working", published: false)
